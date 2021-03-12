e4d01ff7154254e6b2cfca79d39cdedb3c2a5758
Precious Olusola <preciousolusola16@gmail.com>
2021-03-12 12:11:54 +0100

    adjusted environment variables

--- GeniePrep/settings/production.py
+++ GeniePrep/settings/production.py
@@ -3,5 +3,5 @@ from .base import *
 DEBUG = False
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['8c3767f6c672.ngrok.io', '127.0.0.1']
+ALLOWED_HOSTS = ['genieprep.herokuapp.com', '127.0.0.1']
 
--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GeniePrep.settings.development')
     if os.getenv('DJANGO_SETTINGS_MODULE'):
-        os.environ['DJANGO_SETTINGS_MODULE'] = os.getenv('DJANGO_SETTINGS_MODULE')
+        os.environ['DJANGO_SETTINGS_MODULE'] = os.environ.get('DJANGO_SETTINGS_MODULE')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
